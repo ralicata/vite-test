da8ef45d43c2097fe292decc5373ebf91268b328
Roberto Alicata <roberto.alicata@gmail.com>
2022-05-18 18:20:58 +0200

    remove jest. add vitest

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, userEvent } from '../utils/test-utils'
 import App from "../App";
 
-describe("App", () => {
-  it("should work as expected", () => {
-    // render(<App />);
-    expect(1 + 1).toBe(2);
-  });
-});
+describe('Simple working test', () => {
+  it('the title is visible', () => {
+    render(<App />)
+    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument()
+  })
+
+  it('should increment count on click', async () => {
+    render(<App />)
+    userEvent.click(screen.getByRole('button'))
+    expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument()
+  })
+})
