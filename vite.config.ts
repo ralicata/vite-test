213dfa673c8e491a7cba6f4dbefa473cfd473d73
Roberto Alicata <roberto.alicata@gmail.com>
2022-05-19 11:21:34 +0200

    Add test to husky pre-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
+import react from '@vitejs/plugin-react'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [reactRefresh(), tsconfigPaths(), react()],
   test: {
     globals: true,
     watch: false,
